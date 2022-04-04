e654e13ff7f8fec9e9453c1158839fb4c1695bde
DESKTOP-T7VO2G2\Sadicoman <francois.szcz@hotmail.fr>
2022-04-04 14:47:41 +0200

    First commit
    First commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*----- Copyright -----*/
+
+let date = new Date().getFullYear(); 
+
+document.getElementById("year").innerHTML = date;
+
+/*--------------------------------------------*/
+
