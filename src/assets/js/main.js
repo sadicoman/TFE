fd4453dadddff6c98686a11f3a1fd4250dea9eb2
Francois Szczepkowski <francois.szcz@hotmail.fr>
2022-04-06 02:21:10 +0200

    test gsap importe via html
    test gsap importe via html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,18 @@
 "use strict";
 
 /*----- Copyright -----*/
-
+if (document.getElementById("year")) {
 let date = new Date().getFullYear(); 
 
-document.getElementById("year").innerHTML = date;
+document.getElementById("year").innerHTML = date;    
+}
+
+
 
 /*--------------------------------------------*/
 
+gsap.to( '.ball', {
+    x: 300,
+    stagger: 0.08,
+    duration: 1
+} );
