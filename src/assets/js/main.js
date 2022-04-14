9379dcd02a0cbd0765f4c01402059527d10ac029
DESKTOP-T7VO2G2\Sadicoman <francois.szcz@hotmail.fr>
2022-04-14 15:59:22 +0200

    rectification BTN
    rectification BTN

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,13 +6,30 @@ let date = new Date().getFullYear();
 
 document.getElementById("year").innerHTML = date;    
 }
-
-
-
 /*--------------------------------------------*/
 
-gsap.to( '.ball', {
-    x: 300,
-    stagger: 0.08,
-    duration: 1
-} );
+
+/*----- Annimation GSAP -----*/
+// gsap.to( '.ball', {
+//     x: 300,
+//     stagger: 0.08,
+//     duration: 1
+// } );
+/*--------------------------------------------*/
+
+
+/*----- Btn -----*/
+let btnTaille = document.querySelectorAll(".btn");
+let btnTailleHaut = document.querySelectorAll(".btn__el--haut");
+let btnTailleBas = document.querySelectorAll(".btn__el--bas");
+
+for(let i = 0 ; i < btnTaille.length ; i++){ 
+	// .length => nombre d'éléments dans le tableau "btnTaille"
+	let btnHeight = btnTailleHaut[i].offsetHeight;
+	let btnWidth = btnTailleHaut[i].offsetWidth;
+
+	btnTailleBas[i].style.width = btnWidth+"px";
+	btnTailleBas[i].style.height = btnHeight+"px";
+};
+/*--------------------------------------------*/
+
